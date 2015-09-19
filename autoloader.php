d7b5b8d05c3037d457bb230c7cbb7f5d1fae0143
Syu93 <herve.tutuaku@gmail.com>
2015-09-19 19:53:12 +0200

    [feature] new spl autoloader

--- autoloader.php
+++ autoloader.php
@@ -1,12 +1,17 @@
 <?php
 /**
- *	This file is a part of the sophwork project
- *	@Tested version : Sophwork.0.2.9
+ *	This file is a part of the Sophwork project
+ *	@version : Sophwork.0.3.0
  *	@author : Syu93
  *	--
  *	Namespace autoloader
  */
 
+/**
+ * DEPRECATED
+ * @param  [type] $c [description]
+ * @return [type]    [description]
+ */
 function __autoload($c){
 // Autoloader
 	$c = preg_replace("(\\\\)", DIRECTORY_SEPARATOR, $c);
@@ -21,3 +26,18 @@ function __autoload($c){
 		die("Autoload fatal error : ".$e->getMessage());
 	} 
 }
+
+function autoload($className){
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, dirname(__FILE__). DIRECTORY_SEPARATOR . $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
+}
+spl_autoload_register('autoload');
