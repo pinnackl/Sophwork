277b4ae4cf919a09ea768c1974d208ce37ddf2ed
Syu93 <herve.tutuaku@gmail.com>
2015-10-29 12:49:07 +0100

    [doc] complet exemple

--- web/index.php
+++ web/index.php
@@ -16,6 +16,8 @@ use Sophwork\core\Sophwork;
 use Sophwork\app\app\SophworkApp;
 use Sophwork\modules\handlers\errors\errorHandler\ErrorHandler;
 
+use Sophwork\modules\handlers\requests\Requests;
+
 // Set up the source path for the autoloader
 	// Unix env
 // $autoloader->config = '/var/www/Sophwork/src';
@@ -24,43 +26,63 @@ $autoloader->config = __DIR__ . '/../src/';
 
 /*
  *	Create a new applicaion with the Sophwork class
- *	It will create 3 new class :
+ * 	It will create 3 new class :
  *		- appController class
  *		- appModel class
  *		- appView class
+ *
+ * 	Set up app parameters here or in the config file
  */
 $app = new SophworkApp([
-	// 'baseUri' => '', //	base uri
+	// 'baseUri' => '',
 		// Unix env
 	// 'template' => '/var/www/Sophwork/template',
 		// Windows env
 	// 'template' => __DIR__ . '/../template/',
 ]);
 
+/**
+ * Sophwork provide in its module a simple error handle
+ * to display error more readable
+ */
+
+// Force the application to display errors
 // $app->debug = true;
-// $app->inject(new ErrorHandler());
 
-$app->get('/', ['MyApp\Controller\Home' => 'show'], 'home');
-$app->get('/hello/{name}', ['MyApp\Controller\Home' => 'hello'], 'hello');
-// $app->get('/game/{game}', ['MyApp\Controller\Home' => 'gameShow'], 'gameShow');
-// $app->get('/game/{game}/edit', ['MyApp\Controller\Home' => 'gameEdit'], 'gameEdit');
-// $app->get('/game/{category}/{game}', ['MyApp\Controller\Home' => 'gameCategory'], 'gameCategory');
-// $app->post('/form', ['MyApp\Controller\Home' => 'form']);
-// $app->get('/blog/{id}', function($app, $request, $id){
-	// echo'<pre>';
-	// var_dump($id);
-	// echo'</pre>';
-	// return '';
-// });
+$app->inject(new ErrorHandler());
 
-// $app->errors(function (\Exception $e, $errorCode){
-// });
+/**
+ * Simply declare your routes and the patern to match
+ * and link them to the controller in your sources
+ *
+ * You can match the following http requests
+ * 		- get
+ * 		- post
+ * 	You can also attribute them a name so you can use UrlGenerator to create links
+ */
+$app->get('/', ['MyApp\Controller\Home' => 'show'], 'home');	// Separate controller file (recommended)
+$app->get('/hello/{name}', function(SophworkApp $app, requests $request, $name){		// Inline controller
+	return "<h1>Hello " . $name . "</h1>";
+});
+$app->post('/form', ['MyApp\Controller\Home' => 'form']);
 
-// $app->after(function ($app) {
-// 	echo'<pre style="background:#ffffff">';
-// 	var_dump($app);
-// 	echo'</pre>';
-// 	die;
-// });
+/**
+ * Declare your own error handler and customise your message to the user
+ *
+ * NOTE : this only work if your injet the Sophwork errorHandler  in your app !
+ */
+$app->errors(function (\Exception $e, $errorCode){
+	if ($errorCode == 404) {
+		return "<h1>Oups ! =(<br>Jim have died !</h1>";
+	}
+});
+
+$app->before(function ($app, $requests) {
+
+});
+
+$app->after(function ($app, $requests) {
+
+});
 
 $app->run();
