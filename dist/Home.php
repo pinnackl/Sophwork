a6ca4e6d1811ed1d1ed572739d1eef4292916e78
Syu93 <herve.tutuaku@gmail.com>
2015-11-14 19:06:54 +0100

    [doc] clean default controller exemple

--- dist/Home.php
+++ dist/Home.php
@@ -8,63 +8,20 @@ use Sophwork\modules\generators\urls\UrlGenerator;
 class Home
 {
 	public function show(SophworkApp $app, Requests $requests) {
-		$view = $app->appView;
-		$generators = new UrlGenerator();
-		// End up this application if something go wrong
+		// Return direct response
+		return 'Hello World';
 		// 
+		// Render the needed template using the template path configuration
+		// $view = $app->appView;
+		// 
+		// End up this application if something go wrong
 		// return Responses('Error page not found', 404);
 		// or
 		// return $app->abort();
-		return $view->renderView('home');
-	}
-	public function hello(SophworkApp $app, Requests $requests, $name) {
-		// $view = $app->appView;
-		// $generators = new UrlGenerator();
-		// $responses = new Responses('Error page not found', 404);
-		// return $app->abort();
 		// return $view->renderView('home');
+	}
+
+	public function hello(SophworkApp $app, Requests $requests, $name) {
 		return '<h1>Hello ' . $name . '</h1>';
-	}
-	public function blog(SophworkApp $app, Requests $requests, $id) {
-		return new Responses("Blog ID is : <b>" . $id . "</b>");
-	}
-	public function contact(SophworkApp $app) {
-		$view = $app->appView;
-		return $view->renderView('contact', [
-			'baseUrl' => $app->config['baseUrl'],
-		]);
-	}
-	public function gameCategory (SophworkApp $app, Requests $requests, $category, $game){
-		echo'<pre>';
-		var_dump('<b>category</b>');
-		var_dump($category);
-		var_dump('<b>game</b>');
-		var_dump($game);
-		echo'</pre>';
-	}
-	public function gameShow(SophworkApp $app, Requests $requests, $game) {
-		$baseUrl = $app->config['baseUrl'];
-		$view = $app->appView;
-		$app->inject(new UrlGenerator());
-		$editUrl = $app->UrlGenerator->generate('gameEdit', ['game'=>$game]);
-		return $view->renderView('gameShow', [
-			'game' => $game,
-			'editUrl' => $baseUrl.$editUrl,
-		]);
-	}
-	public function gameEdit(SophworkApp $app, Requests $requests, $game) {
-		$baseUrl = $app->config['baseUrl'];
-		$view = $app->appView;
-		$app->inject(new UrlGenerator());
-		$cancel = $app->UrlGenerator->generate('gameShow', ['game'=>$game]);
-		$formAction = $app->UrlGenerator->generate('/form');
-		return $view->renderView('gameEdit', [
-			'game' => $game,
-			'cancel' => $baseUrl.$cancel,
-			'formAction' => $baseUrl.$formAction,
-		]);
-	}
-	public function form(SophworkApp $app, Requests $requests) {
-		return new Responses("Your message :<br><b>" . $requests->get('gameId') . "</b>");
 	}
 }
